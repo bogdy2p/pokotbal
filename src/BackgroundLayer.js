ff7968e97858bd3a4c65101e71107f3043292fa7
Popa <bogdy2p@yahoo.com>
2015-05-29 14:27:40 +0300

    removed backgroundlayer moved code

--- src/BackgroundLayer.js
+++ src/BackgroundLayer.js
@@ -5,20 +5,6 @@ var BackgroundLayer = cc.Layer.extend({
     },
     init: function () {
 
-
-
-//        cc.log(this);
-
-
-//        var all_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//        var howmany = Math.floor((Math.random() * 10) + 1);
-//        var picked_numbers = [];
-//
-//        for (x = 0; x < howmany; x++) {
-//            picked_numbers.push(Math.floor((Math.random() * 10) + 1) - 1);
-//        }
-//        var unique_picked_numbers = _.uniq(picked_numbers);
-
         this._super();
         var winSize = cc.director.getWinSize();
 
@@ -41,84 +27,6 @@ var BackgroundLayer = cc.Layer.extend({
         spriteFootballStripes.setPosition(centerPos);
         this.addChild(spriteFootballStripes, 0, "FootballStripes");
 
-
-//        for (i = 0; i < playerPositions.length; i++) {
-//            //this.addChild(new PlayerLayer(playerPositions[i][0] , playerPositions[i][1]));
-//
-//            var object = playerPositions[i];
-//            var player_x = object.x;
-//            var player_y = object.y;
-//            var player_z = object.zIndex;
-//
-//            switch (i) {
-//
-//                case 0:
-//                    var newPlayer = new cc.Sprite(res.Player1);
-//                    break;
-//                case 1:
-//                    var newPlayer = new cc.Sprite(res.Player2);
-//                    break;
-//                case 2:
-//                    var newPlayer = new cc.Sprite(res.Player3);
-//                    break;
-//                case 3:
-//                    var newPlayer = new cc.Sprite(res.Player4);
-//                    break;
-//                case 4:
-//                    var newPlayer = new cc.Sprite(res.Player5);
-//                    break;
-//                case 5:
-//                    var newPlayer = new cc.Sprite(res.Player6);
-//                    break;
-//                case 6:
-//                    var newPlayer = new cc.Sprite(res.Player7);
-//                    break;
-//                case 7:
-//                    var newPlayer = new cc.Sprite(res.Player8);
-//                    break;
-//                case 8:
-//                    var newPlayer = new cc.Sprite(res.Player9);
-//                    break;
-//                case 9:
-//                    var newPlayer = new cc.Sprite(res.Player10);
-//                    break;
-//                default:
-//                    var newPlayer = new cc.Sprite(res.Player1);
-//                    break;
-//
-//            }
-//            newPlayer.attr({x: player_x, y: player_y});
-//
-//            for (j = 0; j < unique_picked_numbers.length; j++) {
-//
-//                if (i === unique_picked_numbers[j]) {
-////                    this.addChild(newPlayer, player_z);
-//                    
-//                    
-//                    ////////////////////////////////////////////////////////////
-//                    //Add the Tags OVER or UNDER the player.
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    ////////////////////////////////////////////////////////////
-//                    
-//                    
-//                    
-//                }
-//            }
-//        }
     }
 
 
