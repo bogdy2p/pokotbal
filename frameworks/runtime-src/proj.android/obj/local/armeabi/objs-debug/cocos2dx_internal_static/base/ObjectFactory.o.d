70a66d23949c46808a49838caf681798f30012d6
Popa <bogdy2p@yahoo.com>
2015-07-06 14:51:35 +0300

    Commitedframeworks

---
+++ frameworks/runtime-src/proj.android/obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/ObjectFactory.o.d
@@ -0,0 +1,32 @@
+obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/ObjectFactory.o: \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ObjectFactory.cpp \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/functional \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ObjectFactory.h \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCRef.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformMacros.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccConfig.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformConfig.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformDefine.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/android/CCPlatformDefine-android.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/log.h
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/functional:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ObjectFactory.h:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCRef.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformMacros.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccConfig.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformConfig.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformDefine.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/android/CCPlatformDefine-android.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/log.h:
---
+++ frameworks/runtime-src/proj.android/obj/local/armeabi/objs-debug/cocostudio_static/CCObjectExtensionData.o.d
@@ -0,0 +1,236 @@
+obj/local/armeabi/objs-debug/cocostudio_static/CCObjectExtensionData.o: \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/CCObjectExtensionData.cpp \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/CCObjectExtensionData.h \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCRef.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformMacros.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccConfig.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformConfig.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformDefine.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/android/CCPlatformDefine-android.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/log.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/CocosStudioExport.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/ActionTimeline/CCActionTimeline.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/ActionTimeline/CCTimeLine.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/ActionTimeline/CCFrame.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCVector.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccMacros.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCConsole.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/select.h \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/thread \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/mutex \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCStdC.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/android/CCStdC-android.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/math.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/strings.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/memory.h \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/cassert \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccRandom.h \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/random \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCNode.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCProtocols.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccTypes.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/CCGeometry.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/CCMath.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec2.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/CCMathBase.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec2.inl \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec2.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec3.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec3.inl \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec3.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Mat4.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec4.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec4.inl \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Mat4.inl \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec4.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Mat4.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Quaternion.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Quaternion.inl \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCGL.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/android/CCGL-android.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/GLES2/gl2platform.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/KHR/khrplatform.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/GLES2/gl2.h \
+ /home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/GLES2/gl2ext.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCTexture2D.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCScriptSupport.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCCommon.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCTouch.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCEventTouch.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCEvent.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCEventKeyboard.h \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/list \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/CCAffineTransform.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCSprite.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCDrawNode.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCCustomCommand.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCRenderCommand.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCTextureAtlas.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCQuadCommand.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCGLProgramState.h \
+ /home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCTweenFunction.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/ActionTimeline/CCTimelineMacro.h \
+ /home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCAction.h
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/CCObjectExtensionData.h:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCRef.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformMacros.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccConfig.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformConfig.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCPlatformDefine.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/android/CCPlatformDefine-android.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/log.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/CocosStudioExport.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/ActionTimeline/CCActionTimeline.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/ActionTimeline/CCTimeLine.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/ActionTimeline/CCFrame.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCVector.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccMacros.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCConsole.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/select.h:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/thread:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/mutex:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCStdC.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/android/CCStdC-android.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/math.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/cassert:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccRandom.h:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/random:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCNode.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCProtocols.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/ccTypes.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/CCGeometry.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/CCMath.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec2.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/CCMathBase.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec2.inl:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec2.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec3.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec3.inl:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec3.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Mat4.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec4.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec4.inl:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Mat4.inl:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Vec4.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Mat4.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Quaternion.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/Quaternion.inl:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCGL.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/android/CCGL-android.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/GLES2/gl2platform.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/KHR/khrplatform.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/GLES2/gl2.h:
+
+/home/pbc/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/GLES2/gl2ext.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCTexture2D.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCScriptSupport.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./platform/CCCommon.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCTouch.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCEventTouch.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCEvent.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./base/CCEventKeyboard.h:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/list:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./math/CCAffineTransform.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCSprite.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCDrawNode.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCCustomCommand.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCRenderCommand.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCTextureAtlas.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCQuadCommand.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./renderer/CCGLProgramState.h:
+
+/home/pbc/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCTweenFunction.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/editor-support/cocostudio/../cocostudio/ActionTimeline/CCTimelineMacro.h:
+
+/home/pbc/cocos/PokerFotbal/frameworks/runtime-src/proj.android/../../js-bindings/cocos2d-x/cocos/./2d/CCAction.h:
